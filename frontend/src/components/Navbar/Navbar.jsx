20f1cc9a804952ec9867d6e27717350d535353da
Sreehari <sreeharimangalasseri528@gmail.com>
2024-11-22 15:30:39 +0530

    signin  signup frontend

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ProfileInfo from '../Cards/ProfileInfo'
+import { Navigate, useNavigate } from 'react-router-dom'
+
+const Navbar = () => {
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    navigate('/login')
+  }
+
+  return (
+    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
+        <h2 className='text-xl fobnt-medium text-black py-2'>Event Management</h2>
+
+        <ProfileInfo onLogout={onLogout} />
+    </div>
+  )
+}
+
+export default Navbar
